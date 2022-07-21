7f123c19032037cde01b97439d9a038f7b7d5559
BerkayKBL <49043450+BerkayKBL@users.noreply.github.com>
2022-07-21 11:50:01 +0300

    e

--- functions/function.js
+++ functions/function.js
@@ -2,4 +2,10 @@ var randomNumber = Math.floor(Math.random() * 3);
 
 console.log($(".background"))
 
-$(".background").style.backgroundImage = url('./images/background'+randomNumber+'.png')
+$(".background").style.backgroundImage = "url('./images/background'+randomNumber+'.png')"
+
+var e = $(document).getElementsByClassName("background");
+
+console.log(e)
+console.log(e.style)
+e.style.backgroundImage = "url('./images/background'+randomNumber+'.png')"
